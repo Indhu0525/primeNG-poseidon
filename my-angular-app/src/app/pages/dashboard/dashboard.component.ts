c731dff9e15f557507ed635d87090e7673dc0512
Indhu0525 <indu01401@hmail.com>
2024-12-06 19:03:43 +0530

    message...

--- my-angular-app/src/app/pages/dashboard/dashboard.component.ts
+++ my-angular-app/src/app/pages/dashboard/dashboard.component.ts
@@ -8,9 +8,21 @@ import { TrafficchartComponent } from '../../charts/trafficchart/trafficchart.co
 import { PiechartComponent } from '../../charts/piechart/piechart.component';
 import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ChipsModule } from 'primeng/chips';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
+import { ProgressBarModule } from 'primeng/progressbar';
 @Component({
   selector: 'app-dashboard',
-  imports: [BreadcrumbModule,ChartsModule,ProductschartComponent,TrafficchartComponent,PiechartComponent,AvatarGroupModule,AvatarModule],
+  imports: [ProgressBarModule,RatingModule,FormsModule,BreadcrumbModule,ChartsModule,ProductschartComponent,TrafficchartComponent,PiechartComponent,AvatarGroupModule,AvatarModule,OverlayPanelModule,InputGroupModule,InputGroupAddonModule,ButtonModule
+    ,InputTextModule,ChipsModule,CommonModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   standalone: true,
@@ -18,9 +30,14 @@ import { AvatarGroupModule } from 'primeng/avatargroup';
 })
 export class DashboardComponent {
   items: MenuItem[] | undefined;
-
+  rating1: number = 2;//rating
+  rating2: number = 1; //rating 
   home: MenuItem | undefined;
-
+  members = [
+    { name: 'Amy Elsner', image: 'amyelsner.png', email: 'amy@email.com', role: 'Owner' },
+    { name: 'Bernardo Dominic', image: 'bernardodominic.png', email: 'bernardo@email.com', role: 'Editor' },
+    { name: 'Ioni Bowcher', image: 'ionibowcher.png', email: 'ioni@email.com', role: 'Viewer' }
+];
   ngOnInit() {
       this.items = [
           { label: 'DASHBOARD' }, 
